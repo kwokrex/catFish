3730aa05eeed4ceef60ad0948a26595e4bd65a3b
KUMA\rex <>
2023-09-27 00:23:17 +0900

    run the WebSocket server on top of an HTTPS server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,47 @@
+const https = require('https');
+const fs = require('fs');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ noServer: true });
 
-// ... Rest of your code ...
+const serverOptions = {
+  key: fs.readFileSync('catFishServer.key'), 
+  cert: fs.readFileSync('catFishServer.crt'), 
+};
 
-// Create an HTTP server and combine it with the WebSocket server
-const httpServer = require('http').createServer((req, res) => {
-  // Handle HTTP requests if needed
-});
+const server = https.createServer(serverOptions);
+const wss = new WebSocket.Server({ server });
 
-httpServer.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
+let fishState = { x: 0, y: 0, visible: true };
+
+wss.on('connection', (ws) => {
+  console.log("WebSocket connection established...");
+
+  // Handle incoming messages from the client
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+
+    // Check if the message contains screen dimensions
+    if (data.screenWidth && data.screenHeight) {
+      // Update fishState with the provided screen dimensions
+      fishState.screenWidth = data.screenWidth;
+      fishState.screenHeight = data.screenHeight;
+
+      // Generate a random position within the provided dimensions
+      fishState.x = Math.floor(Math.random() * data.screenWidth);
+      fishState.y = Math.floor(Math.random() * data.screenHeight);
+
+      // Broadcast the updated fishState to all clients
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(fishState));
+        }
+      });
+    }
   });
+
+  // Send the initial fishState to the newly connected client
+  ws.send(JSON.stringify(fishState));
 });
 
-// Start both the HTTP server and WebSocket server on port 8080
-httpServer.listen(8080, () => {
-  console.log('HTTP and WebSocket servers are listening on port 8080');
+server.listen(8081, () => {
+  console.log("Secure WebSocket server is listening on port 8081...");
 });
